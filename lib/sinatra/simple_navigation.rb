018a1351c447b39a10ed639848cbc09924953d5a
Andreas Schacke <andreas.schacke@gmail.com>
2015-04-02 11:57:40 +0200

    . make registration work for modular and classic style apps

--- lib/sinatra/simple_navigation.rb
+++ lib/sinatra/simple_navigation.rb
@@ -2,11 +2,17 @@ require 'sinatra/base'
 require 'simple-navigation'
 
 module Sinatra
+  
   module SimpleNavigation
     def self.registered(app)
       app.helpers ::SimpleNavigation::Helpers
       ::SimpleNavigation.register(app)
     end
   end
-  register SimpleNavigation
+  
+  # for classic style apps
+  unless ::Sinatra::Application.root.nil?
+    register SimpleNavigation
+  end
+  
 end
